61e688b5618dd1614b6c12fd24d45d36c15ad6f3
Dean Mundey <dean_mundey@hotmail.com>
2022-07-04 10:23:53 +0100

    Create basic structure of App component and link to index.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 import React from 'react';
+
+import Header from './features/Header/Header';
+import Home from './features/Home/Home';
+import Subreddits from './features/Subreddits/Subreddits';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <Home />
+            </main>
+            <aside>
+                <Subreddits />
+            </aside>
+        </>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+import { store } from './store/store';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
