93261c6f5934ec172623b422c0f6f68f92a6de3f
Dean Mundey <dean_mundey@hotmail.com>
2022-07-04 13:12:14 +0100

    Add subredditsReducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import redditReducer from './redditSlice';
+import subredditsReducer from './subredditSlice';
 
 const store = configureStore({
     reducer: combineReducers({
         reddit: redditReducer,
+        subreddits: subredditsReducer
     }),
 });
 
